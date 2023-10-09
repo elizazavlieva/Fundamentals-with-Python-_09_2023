be758d81c71c3473b040db98c0e94bb842d9d82a
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2023-10-09 09:58:16 +0300

    Solution from Function - More Exercise

---
+++ Functions/Functions - More Exercise/multiplication_sign.py
@@ -0,0 +1,19 @@
+def multiplication(a, b, c):
+    result = None
+    if (c > 0 > a and b < 0) or (b > 0 > a and c < 0) \
+            or (a > 0 > b and c < 0) or (a > 0 and b > 0 and c > 0):
+        result = "positive"
+
+    elif a == 0 or b == 0 or c == 0:
+        result = "zero"
+
+    elif a < 0 or b < 0 or c < 0:
+        result = "negative"
+
+    return result
+
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+print(multiplication(first_number, second_number, third_number))
